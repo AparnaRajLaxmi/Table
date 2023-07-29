8519496d4ba56c6f9bbe6b2540ad919dab797bfe
Ankit <114883565+Ankitsatyam@users.noreply.github.com>
2023-07-30 01:41:44 +0530

    Adding navigate on homepage feature

--- frontend/src/components/UpdateEmployee.jsx
+++ frontend/src/components/UpdateEmployee.jsx
@@ -1,12 +1,12 @@
-import React  from 'react'
+import React from 'react'
 import { useForm } from 'react-hook-form';
-import { useNavigate,useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 
 function UpdateEmployee() {
 
   const param = useParams()
-  
+
 
   const navigate = useNavigate();
   const { register, handleSubmit } = useForm();
@@ -14,7 +14,7 @@ function UpdateEmployee() {
   const createEmployee = async (data) => {
 
     const savedUserResponse = await fetch(
-      `${process.env.REACT_APP_BASE_URL}/updateUser/${param.id}` ,
+      `${process.env.REACT_APP_BASE_URL}/updateUser/${param.id}`,
       {
         method: "PUT",
         headers: {
@@ -29,7 +29,8 @@ function UpdateEmployee() {
     navigate("/")
   };
 
-  return(
+
+  return (
     <div>
       <form onSubmit={handleSubmit(createEmployee)} className="mt-8">
         <div className="space-y-5">
@@ -43,7 +44,7 @@ function UpdateEmployee() {
             </label>
             <div className="mt-2.5">
               <input
-               className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
                 type="text"
                 placeholder="Enter You Updated Full Name"
                 {...register("name")}
@@ -96,7 +97,7 @@ function UpdateEmployee() {
             </label>
             <div className="mt-2.5">
               <input
-               className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
                 type="text"
                 placeholder="Enter Your Updated Employee Department"
                 {...register("department")}
@@ -108,7 +109,7 @@ function UpdateEmployee() {
             <label
               htmlFor="role"
               className="text-base font-medium text-gray-900 dark:text-gray-200"
-           >
+            >
               {" "}
               Updated Employee Role{" "}
             </label>
@@ -125,7 +126,7 @@ function UpdateEmployee() {
           <div>
             <button
               type="submit"
-             className='className="inline-flex w-full items-center justify-center rounded-md bg-indigo-600 px-3.5 py-2.5 text-base font-semibold leading-7 text-white hover:bg-indigo-500"'
+              className="inline-flex w-full items-center justify-center rounded-md bg-indigo-600 px-3.5 py-2.5 text-base font-semibold leading-7 text-white hover:bg-indigo-500"
             >
               Update Employeee
               <svg
@@ -143,6 +144,14 @@ function UpdateEmployee() {
                 />
               </svg>
             </button>
+            <Link to={`/`}>
+              <div>
+                <button className=" text-white align-middle mt-5 rounded-md bg-slate-800 p-2 hover:bg-black">
+                  Go Back
+
+                </button>
+              </div>
+            </Link>
           </div>
         </div>
       </form>
